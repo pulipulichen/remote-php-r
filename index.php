92e98bb61f8e5954bd8c90f290f7e3a0d205e3ce
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-11-08 11:02:48 +0800

    1108 好啦測試

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$rscript_exec = 'D:/Program Files/R/R-3.0.2/bin/RScript.exe';
+
+if (strpos($rscript_exec, " ") !== 0) {
+    $rscript_exec = '"' . $rscript_exec . '"';
+}
+
+header('Content-Type: application/json; charset=utf-8');
+$callback = $_GET["callback"];
+$d = $_GET["d"];
+
+// 把$d寫入檔案
+$filename = tempnam(sys_get_temp_dir(), "rscript");
+file_put_contents($filename, $d);
+exec($rscript_exec . " " . $filename, $output);
+
+//$result = json_encode($d);
+$result = $output;
+$result = json_encode($result);
+
+echo $callback.'(' . $result .');';
+//echo $_GET["d"];
+//echo 1;
